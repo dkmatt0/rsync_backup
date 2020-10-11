8a25e27df0fd58d1aebd09cf002e9d9898397d0c
dkmatt0 <dkmatt0@gmx.fr>
2020-10-11 11:40:59 +0200

    ajout des logs

--- rotate.py
+++ rotate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
 
 
 ## définition des arguments du programmme
@@ -14,3 +15,25 @@ parser.add_argument("-v", "--verbose", action="count", help="increase output ver
 parser.add_argument("-b", "--backupdir", help="home backup directory (current directory by default)")
 parser.add_argument("-n", "--dry-run", action="store_true", help="dry run")
 args = parser.parse_args()
+
+## définition des niveaux de log
+loglevel_list = (logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG)
+
+## gestion de la verbosité
+if args.quiet:
+  loglevel_min = 1
+else:
+  loglevel_min = 3
+
+if args.verbose + loglevel_min >= 5:
+  loglevel = loglevel_list[4]
+elif args.verbose + loglevel_min >= 1:
+  loglevel = loglevel_list[args.verbose + loglevel_min - 1]
+else:
+  loglevel = loglevel_list[loglevel_min]
+
+## activation des logs
+logging.basicConfig(
+  filename=None, format="[%(asctime)s] %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=loglevel
+)
+
